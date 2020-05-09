345dab6cabfd111108651598f4a06b5af6c4dc93
robalty <32559243+robalty@users.noreply.github.com>
2020-05-08 22:17:02 -0700

    Changed models and views to match schema

--- django/todoapi/admin.py
+++ django/todoapi/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post, Community, User
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Community)
--- django/todoapi/models.py
+++ django/todoapi/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 from datetime import date
 
-# Create Ask class to describe the model
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    created_on = models.DateField(default=date.today)
+    profile = models.CharField(max_length=160)
+
+    class Meta:
+        db_table = 'user'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
-    """Stores a person's 'Ask' (e.g. I need milk)"""
     title = models.CharField(max_length=50)
     created_on = models.DateField(default=date.today)
+    owner = models.ForeignKey('User',  on_delete=models.CASCADE)
     
     # Meta data about DB table
     class Meta:
@@ -16,3 +27,15 @@ class Post(models.Model):
     # What to output when model is printed as a String
     def __str__(self):
         return self.title
+class Community(models.Model):
+    """"""
+    name = models.CharField(max_length=50)
+    created_on = models.DateField(default=date.today)
+    members = models.ManyToManyField(User, related_name='member_of')
+    
+    class Meta:
+        db_table = 'community'
+        ordering = ['id']
+    
+    def __str__(self):
+        return self.name
