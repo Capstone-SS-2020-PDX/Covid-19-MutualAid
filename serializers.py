80107a21586fa8ad59b6e73fb90b6c3bdcee8c97
robalty <32559243+robalty@users.noreply.github.com>
2020-05-21 17:57:57 -0700

    Changed posting model

---
+++ serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Posting, Community, User
+
+class PostingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Posting
+        fields = ('id', 'title', 'desc', 'count', 'category', 'created_on', 'owner')
+
+class CommunitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Community
+        fields = ('id', 'name', 'created_on', 'members')
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'profile_text', 'created_on', 'member_of', 'phone_number')
