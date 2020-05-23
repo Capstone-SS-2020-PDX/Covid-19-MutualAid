9136b16c6f591f385a032aa95533a7f0ba2545cf
Simon Barton <Simon5@pdx.edu>
2020-05-22 20:06:39 -0700

    set postman tests to run against a local fresh django

--- pre-push.sh
+++ pre-push.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-cd django/tests && pytest -vvv && cd ../../react-native && npm run test && cd ../qa && newman run suite.postman_collection.json
+
+set -x
+
+# Python unit test
+# Start local django / db in background, give it time to start
+# JS unit test
+# Run Postman tests against local django (new code)
+# Kill docker containers in background
+
+cd django/tests && pytest -vvv \
+&& cd ../.. && (make server > /dev/null 2>&1 &) \
+&& sleep 5 \
+&& cd react-native && npm run test \
+&& cd ../qa && newman run suite.postman_collection.json \
+&& docker ps -a -q
