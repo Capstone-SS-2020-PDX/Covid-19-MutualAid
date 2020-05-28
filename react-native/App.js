9e47ba66facc94a9645c47842471a67b73ff64db
Sean Sisson <sisson@pdx.edu>
2020-05-27 17:42:35 -0700

    Add basics of react-navigation to app

--- react-native/App.js
+++ react-native/App.js
@@ -1,12 +1,41 @@
 "use strict";
 
-import React from "react";
+import 'react-native-gesture-handler';
+import * as React from "react";
 import {StyleSheet, Text, View, ActivityIndicator, Button} from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import PostingCreationScreen from './components/PostingCreationScreen';
-import PostingListScreen from './components/PostingListScreen'
+import PostingListScreen from './components/PostingListScreen';
 
 const url = "https://cellular-virtue-277000.uc.r.appspot.com"
 const local_url = "http://localhost"
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default class App extends React.Component {
 
@@ -44,18 +73,20 @@ export default class App extends React.Component {
   render() {
     if (this.state.isLoading) {
       return (
-        <View style={styles.container}>
-          <ActivityIndicator />
-        </View>
+        <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Loading" component={ActivityIndicator} />
+        </Stack.Navigator>
+      </NavigationContainer>
       )
     } else {
       return (
-        <View style={styles.container}>
-          <Text>{this.state.dataSource[0].title}</Text>
-        
-          <Button onPress={load_posting_screenfoobar} title="Posting List Screen"></Button>
-          <Button onPress={load_posting_creation_screen} title="Posting creation screen"></Button>
-        </View>
+        <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
       );
     }
   }
