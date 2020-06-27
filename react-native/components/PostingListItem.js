aaa0366627af3142a4fab860d2889d426896eab7
Erik Haake <ehaake@pdx.edu>
2020-06-27 13:44:39 -0700

    Add image and icons to PostingListItem

--- react-native/components/PostingListItem.js
+++ react-native/components/PostingListItem.js
@@ -9,14 +9,36 @@ import { Text,
 import Colors from '../config/colors';
 
 const PostingListItem = props => {
+    const offeredItemIconImage = '../assets/offered_item.png';
+    const requestedItemIconImage = '../assets/requested_item.png';
+
     return(
         <TouchableOpacity
           style={{ ...styles.listItem, ...props.style }}
           onPress={props.onSelectPosting}
         >
-            <Text style={styles.text}>
-              {props.title}
-            </Text>
+          <View style={styles.itemContent}>
+            <Image
+              style={styles.listImage}
+              resizeMode='contain'
+              source={{uri: 'https://source.unsplash.com/random/80x80'}}
+            />
+            <View style={styles.itemTextContent}>
+              <Text style={styles.itemTitleText}>
+                {props.title}
+              </Text>
+              <Text style={styles.itemCommunityText}>
+                Oak Grove
+              </Text>
+            </View>
+            <View style={styles.postingTypeIconContainer}>
+              <Image
+                style={styles.postingTypeIconImage}
+                resizeMode='contain'
+                source={require(offeredItemIconImage)}
+              />
+            </View>
+          </View>
         </TouchableOpacity>
     );
 };
@@ -32,9 +54,27 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
 
     },
-    text: {
+    itemContent: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    itemTextContent: {
+        alignContent: 'flex-start',
+    },
+    listImage: {
+        width: 80,
+        height: 80,
+        borderWidth: 1,
+        borderColor: Colors.dark_shade1,
+    },
+    itemTitleText: {
         fontSize: 20,
-    }
+        fontFamily: 'open-sans',
+    },
+    postingTypeIconImage: {
+        width: 80,
+        height: 80,
+    },
 });
 
 export default PostingListItem;
