0f8cc82fa0bc87c477a6ffd693ca7f1eb20362b5
Simon Barton <Simon5@pdx.edu>
2020-06-26 20:26:52 -0700

    Switch yarn installs to npm
    Remove yarn install entirely
    
    expo install global
    
    Remove newman package
    
    set -x to debug .........
    
    npm is not intuitive

--- qa/prereq-linux2.sh
+++ qa/prereq-linux2.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
+set -x
+
 echo "$(tput setaf 2)Log: Beginning make install part 2 ...$(tput sgr 0)"
 
 cd "react-native/"
-npm install -g yarn
-npm install --global expo-cli
-yarn add expo
-yarn add @react-navigation/native
-yarn add --dev jest
+npm install expo-cli --global
+npm install @react-navigation/native
+npm install
+npm install expo --global
 
 expo install react-native-gesture-handler react-native-reanimated \
             react-native-screens react-native-safe-area-context \
             @react-native-community/masked-view
 
-
-
-# Could not find yarn option for newman
-npm install -g newman
-npm install
-
 export PATH=$PATH:~/.npm-global/bin
+
+cd..
+npm install expo-cli --global
+
 echo "Finished..."
