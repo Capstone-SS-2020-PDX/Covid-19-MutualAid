f55be221a7cd93f576209efa99c2503617120aec
robalty <32559243+robalty@users.noreply.github.com>
2020-06-28 15:12:14 -0700

    AttributeError

--- django/cgproj/urls.py
+++ django/cgproj/urls.py
@@ -28,5 +28,4 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
-    path('contact/', cgapi_views.UserContact),
 ]
