fac202541de18a12d685f0ff9fe683671ba486e6
robalty <32559243+robalty@users.noreply.github.com>
2020-06-29 18:18:49 -0700

    Model and serializer changes
    Added email field to posting
    Fixed User serialization within UserProfile

--- django/cgapi/serializers.py
+++ django/cgapi/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from .models import Posting, Community, UserProfile, User
 
 class PostingSerializer(serializers.ModelSerializer):
+    parser_classes = (MultiPartParser, FormParser, JSONParser)
     class Meta:
         model = Posting
         fields = ('id', 'title', 'desc', 'count', 'category', 'created_on', 'owner', 'in_community', 'item_pic', 'request')
@@ -14,11 +16,10 @@ class CommunitySerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('username', 'first_name', 'last_name', 'email', 'groups', 'is_active', 'date_joined', 'last_login')
 
 class UserProfileSerializer(serializers.ModelSerializer):
-    qs = User.objects.all()
-    user = serializers.RelatedField(queryset=qs)
+    related_user = UserSerializer(source='user')
     class Meta:
-        model = UserProfile
-        fields = ('id', 'user', 'profile_text', 'created_on', 'home', 'member_of', 'phone_number', 'profile_pic')
+        model = UserProfile	
+        fields = ('id', 'related_user', 'profile_text', 'created_on', 'home', 'member_of', 'phone_number', 'profile_pic')
