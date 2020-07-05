79fef5b9020e8b8f6db82f80821208dd5868f878
robalty <32559243+robalty@users.noreply.github.com>
2020-07-04 17:22:09 -0700

    Added token auth and user registration

--- django/cgapi/views.py
+++ django/cgapi/views.py
@@ -1,13 +1,19 @@
-import rest_framework as rf
-from rest_framework.decorators import action
+import logging
+import json
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import action, api_view, authentication_classes, permission_classes, renderer_classes
+from rest_framework.renderers import JSONRenderer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.views import APIView
 from django.contrib.auth import get_user
 from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
 from .models import Posting, Community, UserProfile
 from .serializers import PostingSerializer, CommunitySerializer, UserProfileSerializer, UserSerializer
-from rest_framework import viewsets, permissions
-
-
-class PostingViewSet(rf.viewsets.ModelViewSet):
+        
+class PostingViewSet(ModelViewSet):
     queryset = Posting.objects.all()
     serializer_class = PostingSerializer
     
@@ -16,26 +22,45 @@ class PostingViewSet(rf.viewsets.ModelViewSet):
         argument = request.query_params.get('title', '')
         posting = Posting.objects.filter(title__contains=argument)
         serializer = PostingSerializer(posting, many=True)
-        return rf.response.Response(serializer.data)
+        return Response(serializer.data)
     
     @action(detail=False)
-    def auth(self, request):
-        user = get_user(request)
-        if user.is_authenticated:
+    @api_view(['GET'])
+    @authentication_classes([TokenAuthentication])
+    @permission_classes([IsAuthenticated])
+    def auth(request):
             posting = Posting.objects.all()
             serializer = PostingSerializer(posting, many=True)
-            return rf.response.Response(serializer.data)
-        else:
-            return rf.response.Response(status=400)
+            return Response(serializer.data)
+            
+    @action(detail=False, methods=['POST'])
+    def contact(self, request):
+        if request.method == 'POST':
+            post_id = request.data.get('postid', '')
+            related_post = Posting.objects.get(pk=post_id)
+            recipient = getattr(related_post, 'email')
+            post_title = getattr(related_post, 'title')
+            post_desc = getattr(related_post, 'desc')
+            sender = request.data.get('addressfrom', '')
+            subj = "Common Goods test email"
+            message = "Hey there! \n\nSomebody's interested in your post:\nTitle: %s \nDescription: %s \n" % (post_title, post_desc)
+            email = EmailMessage(
+                subject=subj,
+                body=message,
+                to=[recipient],
+                reply_to=[sender]
+            )
+            email.send()
+        return Response(request.data)
 
-class CommunityViewSet(rf.viewsets.ModelViewSet):
+class CommunityViewSet(ModelViewSet):
     """
     API endpoint allowing Community objects to be created, viewed, edited, deleted
     """
     queryset = Community.objects.all()
     serializer_class = CommunitySerializer
 
-class UserProfileViewSet(rf.viewsets.ModelViewSet):
+class UserProfileViewSet(ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
 
@@ -44,8 +69,20 @@ class UserProfileViewSet(rf.viewsets.ModelViewSet):
         argument = request.query_params.get('member_of', '')
         user = UserProfile.objects.filter(member_of__id=argument)
         serializer = UserProfileSerializer(userprofile, many=True)
-        return rf.response.Response(serializer.data)
+        return Response(serializer.data)
         
-class UserViewSet(rf.viewsets.ModelViewSet):
+class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    
+@api_view(('POST',))
+@renderer_classes((JSONRenderer,))
+def register_user(request):
+    post_data = json.loads(request.body)
+    serializer = UserSerializer(data=post_data)
+    if serializer.is_valid(raise_exception=True):
+        user = serializer.save()
+        userpw = serializer.validated_data.get('password')
+        user.set_password(userpw)
+        return Response(data=serializer.validated_data)
+    return Response(data=serializer.errors)    
