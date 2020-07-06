801182d0ca5ca309f3f89c7c6e19e2846b50243e
Erik Haake <ehaake@pdx.edu>
2020-07-06 14:50:41 -0700

    Add ProfileScreen to screens directory

---
+++ react-native/screens/ProfileScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    StyleSheet,
+} from 'react-native';
+
+const ProfileScreen = props => {
+    return();
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default ProfileScreen;
