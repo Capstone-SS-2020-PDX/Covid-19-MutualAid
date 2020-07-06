ef7566f5b33108d4735dd91792a471df6403deac
Erik Haake <ehaake@pdx.edu>
2020-07-06 15:03:17 -0700

    Add ProfileStack to Navtabs and add icons

--- react-native/screens/ProfileEditScreen.js
+++ react-native/screens/ProfileEditScreen.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import {
+    View,
+    Text,
     StyleSheet,
 } from 'react-native';
 
 const ProfileEditScreen = props => {
-    return();
+    return(
+        <View style={styles.screen}>
+          <Text>Edit Profile Screen</Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+    },
 });
 
 export default ProfileEditScreen;
