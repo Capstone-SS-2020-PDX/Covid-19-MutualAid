76c18440d2bdb50a30327e81609a9ebbb8b47980
robalty <32559243+robalty@users.noreply.github.com>
2020-07-09 15:58:50 -0700

    Add files via upload

--- django/cgproj/urls.py
+++ django/cgproj/urls.py
@@ -17,18 +17,18 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from cgapi import views as cgapi_views
-from rest_framework.authtoken import views as auth_views
 
 router = routers.DefaultRouter()
 router.register(r'communities', cgapi_views.CommunityViewSet)
 router.register(r'postings', cgapi_views.PostingViewSet)
-router.register(r'users', cgapi_views.UserProfileViewSet)
+router.register(r'profiles', cgapi_views.UserProfileViewSet)
 router.register(r'auth', cgapi_views.UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
-    path('token/', auth_views.obtain_auth_token),
-    path('register/', cgapi_views.register_user)
+    path('token/', cgapi_views.CustomAuthToken.as_view()),
+    path('register/', cgapi_views.register_user),
+    path('check-username/', cgapi_views.username_available),
 ]
