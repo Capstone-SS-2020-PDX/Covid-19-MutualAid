4c5a96f8257e8756f36c316611b9fb029ff692c7
Erik Haake <ehaake@pdx.edu>
2020-07-22 15:33:00 -0700

    Add JoinCommunities Screen/List components

---
+++ react-native/components/JoinCommunitiesList.js
@@ -0,0 +1,59 @@
+import React, { useContext, useState, useLayoutEffect, useEffect } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, FlatList, RefreshControl } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+
+import CommunityListItem from './CommunityListItem';
+import { AuthContext } from '../providers/AuthProvider';
+
+import Colors from '../config/colors';
+
+
+const JoinCommunitiesList = props => {
+  const { navigation, communities } = props;
+  const { user } = useContext(AuthContext);
+
+  console.log("Inside CommunityList, communitySelections: ");
+  console.log(props.communitySelections);
+  const renderCommunityListItem = itemData => {
+    // let isMemberOf = props.communitySelections[itemData.item.id];
+    return(
+      <View style={styles.list}>
+        <CommunityListItem
+          name={itemData.item.name}
+          home_pic={itemData.item.home_pic}
+          members={itemData.item.members}
+          postings={itemData.item.posts}
+          id={itemData.item.id}
+          /* isSelected={isMemberOf} */
+          onSelectCommunity={() => {
+            /* props.updateCommunitySelections(itemData.item.id, isMemberOf); */
+          }}
+        />
+      </View>
+    );
+  };
+
+  return(
+    <FlatList
+      style={styles.list}
+      renderItem={renderCommunityListItem}
+      keyExtractor={(itemData, i) => i.toString()}
+      data={props.communities}
+      showsVerticalScrollIndicator={false}
+      /* refreshControl={ */
+      /*   <RefreshControl */
+      /*     refreshing={props.isLoading} */
+      /*     onRefresh={props.onRefresh} */
+      /*   /> */
+      /* } */
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  list: {
+    width: '100%',
+  },
+});
+
+export default JoinCommunitiesList;
---
+++ react-native/screens/JoinCommunitiesScreen.js
@@ -0,0 +1,181 @@
+import React, { useContext, useState, useRef, useEffect, useLayoutEffect } from 'react';
+import { View,
+         Text,
+         TextInput,
+         StyleSheet,
+         ActivityIndicator,
+         TouchableOpacity,
+       } from 'react-native';
+import { Ionicons, Feather } from '@expo/vector-icons';
+
+import JoinCommunitiesList from '../components/JoinCommunitiesList';
+import Colors from '../config/colors';
+import { windowWidth, windowHeight } from '../config/dimensions';
+
+import { AuthContext } from '../providers/AuthProvider';
+
+const JoinCommunitiesScreen = props => {
+    const { user, communities } = useContext(AuthContext);
+    const { navigation } = props;
+
+    console.log(user.profile.member_of);
+    const communityList = communities.filter(community => {
+        return community.id !== user.profile.home;
+    });
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [searchCommunities, setSearchCommunities] = useState(communityList);
+    const [communitySelections, setCommunitySelections] = useState({});
+    const [searchText, setSearchText] = useState('');
+    const searchInputRef = useRef(null);
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <TouchableOpacity
+                  style={styles.joinButton}
+                  onPress={() => handleJoinCommunities() }
+                >
+                  <Text style={styles.joinButtonText}>Join</Text>
+                </TouchableOpacity>
+            ),
+        });
+
+    }, [navigation]);
+
+    // useEffect(() => { initializeCommunitySelections() }, []);
+
+    // const initializeCommunitySelections = () => {
+    //     let selections = {};
+
+    //     communities.map((community, index) => {
+    //         selections[community.id] = community.id === user.profile.member_of.find(id => id === community.id);
+    //         console.log(selections[community.id]);
+    //     });
+
+    //     setCommunitySelections(selections);
+    //     console.log("selection status from initializeCommunitySelections: " + JSON.stringify(communitySelections));
+    // };
+
+
+    // const updateCommunitySelections = (id, value) => {
+    //     console.log("id: " + id + " value: " + value);
+    //     setCommunitySelections({
+    //         ...communitySelections,
+    //         [id]: !value,
+    //     });
+    // };
+
+    const handleJoinCommunities = () => {
+        console.log('joining communities');
+    };
+
+    const handleSearch = text => {
+        setSearchText(text);
+
+        let filteredCommunities = communities.filter(community =>
+            community.name.toLowerCase().includes(text.toLowerCase())
+        );
+
+        setSearchCommunities(filteredCommunities);
+    };
+
+    const handleClearSearchInput = () => {
+        setSearchText('');
+        setSearchCommunities(communities);
+        searchInputRef.current.clear();
+    };
+
+    const CommunityListSection = isLoading
+          ? <ActivityIndicator size='large'/>
+          : <JoinCommunitiesList
+              navigation={navigation}
+              isLoading={isLoading}
+              communities={searchCommunities}
+              communitySelections={communitySelections}
+              /* updateCommunitySelections={updateCommunitySelections} */
+    /* onRefresh={fetchCommunities} */
+            />
+
+    return(
+        <View style={styles.screen}>
+          <View style={styles.searchContainer}>
+            <View style={styles.inputView}>
+              <Ionicons
+                name={'ios-search'}
+                size={23}
+                style={styles.inputIcon}
+              />
+              <TextInput
+                style={styles.inputText}
+                placeholder='Search for a community...'
+                placeholderTextColor={Colors.placeholder_text}
+                autoCapitalize='none'
+                onChangeText={text => handleSearch(text)}
+                returnKeyType='done'
+                ref={searchInputRef}
+              />
+              <TouchableOpacity
+                style={styles.inputIcon}
+                onPress={() => handleClearSearchInput()}
+              >
+                <Feather
+                  name={'x-circle'}
+                  size={20}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          {CommunityListSection}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        height: '100%',
+        backgroundColor: Colors.light_shade4,
+        alignItems: 'center',
+    },
+    searchContainer: {
+        width: '90%',
+        marginVertical: windowHeight / 80,
+    },
+    inputView: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+        backgroundColor: Colors.light_shade4,
+        borderRadius: 25,
+        borderColor: Colors.placeholder_text,
+        borderWidth: 0.5,
+        paddingLeft: 20,
+        paddingRight: 10,
+        shadowColor: Colors.dark_shade1,
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    inputText: {
+        height: 50,
+        width: '80%',
+        color: Colors.dark_shade1,
+    },
+    inputIcon: {
+        width: '10%',
+    },
+    joinButton: {
+        paddingRight: 15,
+    },
+    joinButtonText: {
+        color: Colors.contrast1,
+        fontSize: 16,
+    },
+});
+
+export default JoinCommunitiesScreen;
