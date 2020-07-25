719c07c825988c82cc887881c62ec88b46f2f9b4
Erik Haake <ehaake@pdx.edu>
2020-07-25 16:11:53 -0700

    navigating to profile screen should no longer crash if there is no home community

--- react-native/screens/ProfileScreen.js
+++ react-native/screens/ProfileScreen.js
@@ -23,7 +23,7 @@ const ProfileScreen = props => {
   const { user, communities } = useContext(AuthContext);
 
   const picUrl = user.profile.profile_pic ? user.profile.profile_pic : null;
-  const homeCommunity = communities.find(community => community.id === user.profile.home);
+  const homeCommunity = communities.find(community => community.id === user.profile.home) || '';
 
   return(
     <ScrollView contentContainerStyle={styles.screen}>
