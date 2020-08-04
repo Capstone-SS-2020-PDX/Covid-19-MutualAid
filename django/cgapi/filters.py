602a8a70b03c05f73710224efa7da6b4d8fca91c
Sean <sisson@pdx.edu>
2020-08-03 18:55:26 -0700

    Add community filter

--- django/cgapi/filters.py
+++ django/cgapi/filters.py
@@ -6,10 +6,11 @@ from django_filters import rest_framework as filters
 
 class PostingFilter(filters.FilterSet):
     radius = filters.CharFilter(method='radius_filter')
+    communities = filters.CharFilter(method='communities_filter')
 
     class Meta:
         model = Posting
-        fields = ['radius',]
+        fields = ['radius', 'communities',]
 
     def radius_filter(self, queryset, name, value):
         """Filter postings in a radius about a given longitude/latitude"""
@@ -20,5 +21,14 @@ class PostingFilter(filters.FilterSet):
             radius = int(rad)
             point = Point(float(lng), float(lat))
             return queryset.filter(location__distance_lt=(point, Distance(mi=radius)))
+        return queryset.none()
+
+    def communities_filter(self, queryset, name, value):
+        """Filter postings by a list of community IDs"""
+        communities = self.request.GET.get('communities')
+        if communities is not None:
+            # Cast all strings to integers in list
+            ids = [int(i) for i in communities.split(',')]
+            return queryset.filter(in_community__in=ids)
         return queryset.none()
 
