46e9704d3d9bcec48bd91651ec3ba6758ac68549
Erik Haake <ehaake@pdx.edu>
2020-08-04 15:46:53 -0700

    Modify PostingList to use postings in context instead of from props

--- react-native/components/PostingList.js
+++ react-native/components/PostingList.js
@@ -1,43 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet, FlatList, RefreshControl } from 'react-native';
 
 import PostingListItem from '../components/PostingListItem';
+import { AuthContext } from '../providers/AuthProvider';
 
 const PostingList = props => {
-    const { postings, navigation } = props;
+  const { navigation } = props;
+  const { postings } = useContext(AuthContext);
 
-    const renderPostingListItem = itemData => {
-        return(
-          <View style={styles.list}>
-            <PostingListItem
-              title={itemData.item.title}
-              request={itemData.item.request}
-              item_pic={itemData.item.item_pic}
-              in_community={itemData.item.in_community}
-              location={itemData.item.location}
-              flagged={itemData.item.flagged}
-              onSelectPosting={() => {
-                  navigation.navigate('PostingDetail', {
-                    title: itemData.item.title,
-                    description: itemData.item.desc,
-                    userId: itemData.item.userId,
-                    id: itemData.item.id,
-                    category: itemData.item.category,
-                    count: itemData.item.count,
-                    owner: itemData.item.owner,
-                    created_on: itemData.item.created_on,
-                    item_pic: itemData.item.item_pic,
-                    request: itemData.item.request,
-                    in_community: itemData.item.in_community,
-                    moderatorView: props.moderatorView,
-                    location: itemData.item.location,
-                    flagged: itemData.item.flagged,
-                  });
-              }}
-            />
-          </View>
-        );
-    };
+  const renderPostingListItem = itemData => {
+    return(
+      <View style={styles.list}>
+        <PostingListItem
+          title={itemData.item.title}
+          request={itemData.item.request}
+          item_pic={itemData.item.item_pic}
+          in_community={itemData.item.in_community}
+          location={itemData.item.location}
+          flagged={itemData.item.flagged}
+          onSelectPosting={() => {
+            navigation.navigate('PostingDetail', {
+              title: itemData.item.title,
+              description: itemData.item.desc,
+              userId: itemData.item.userId,
+              id: itemData.item.id,
+              category: itemData.item.category,
+              count: itemData.item.count,
+              owner: itemData.item.owner,
+              created_on: itemData.item.created_on,
+              item_pic: itemData.item.item_pic,
+              request: itemData.item.request,
+              in_community: itemData.item.in_community,
+              moderatorView: props.moderatorView,
+              location: itemData.item.location,
+              flagged: itemData.item.flagged,
+            });
+          }}
+        />
+      </View>
+    );
+  };
 
   return(
     <FlatList
